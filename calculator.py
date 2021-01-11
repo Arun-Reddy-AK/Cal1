82a8be26c69956538a48b8e029d7668cf8095340
Arun Reddy <arunak9100@gmail.com>
2021-01-11 21:45:46 +0530

    added add,sub,mul,div features

--- calculator.py
+++ calculator.py
@@ -1,1 +1,28 @@
-print("Calculator Project")
+
+class Calculator:
+
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def add(self):
+        print("Addition of A and B  is :",self.a+self.b)
+
+    def sub(self):
+        print("Substraction of A and B is:",self.a-self.b)
+
+    def mul(self):
+        print("Multiplication of A and B is:",self.a*self.b)
+
+    def div(self):
+        print("Division of A and B is: ",self.a/self.b)
+
+    def modu(self):
+        print("Modular division of A and B is: ",self.a%self.b)
+
+
+obj=Calculator(a=int(input("Enter the value for A")),b=int(input("Enter the value for B")))
+
+obj.add()
+
+
